41fab5c5f74a5edbdd926b897ebdca5bf0ec153b
Onur Sumakoğlu <sumakogluonur@gmail.com>
2022-01-03 20:11:34 +0300

    first commit

---
+++ app/src/main/java/com/onursumakoglu/cookrecipes/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.onursumakoglu.cookrecipes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+    }
+}
